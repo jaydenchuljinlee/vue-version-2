a046e33c196af13ea01d266eb04d5bacd0a05109
jaydenchuljinlee <cheoljin1234@naver.com>
2020-08-08 17:32:40 +0900

    router default

--- src/main.js
+++ src/main.js
@@ -1,30 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-/* eslint-disable */
-Vue.component('button-counter', {
-  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
-  data: function () {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    incrementCounter: function () {
-      this.counter += 1
-      this.$emit('increment')
-    }
-  },
-})
+import App from './App' 
+import router from './router'
 
-new Vue({
-  el: '#counter-event-example',
-  data: {
-    total: 0
-  },
-  methods: {
-    incrementTotal: function () {
-      this.total += 1
-    }
-  }
-})
+Vue.config.productionTip = false 
+
+/* eslint-disable no-new */
+new Vue({ 
+  el: '#app', 
+  router, 
+  components: { App }, 
+  template: '<App/>'
+ })
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/test'
+import Currency from '@/components/currency'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/test',
       name: 'Test',
       component: Test
+    },
+    {
+      path: '/currancy',
+      name: 'Currancy',
+      component: Currency
     }
   ]
 })
